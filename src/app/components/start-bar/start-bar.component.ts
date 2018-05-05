ba502758ed6aecc2630a5a93f410fb006ed69812
Jan Kuri <jkuri88@gmail.com>
2018-05-05 22:39:41 +0200

    feat(): click outside of menu

--- src/app/components/start-bar/start-bar.component.ts
+++ src/app/components/start-bar/start-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-start-bar',
@@ -8,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class StartBarComponent implements OnInit {
   startBarOpened: boolean;
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
   ngOnInit() {
   }
 
+  @HostListener('document:click', ['$event']) onBlur(e: MouseEvent) {
+    if (!this.startBarOpened) {
+      return;
+    }
+
+    const input = this.elementRef.nativeElement.querySelector('.start-button');
+
+    if (input == null) {
+      return;
+    }
+
+    if (e.target === input || input.contains(<any>e.target)) {
+      return;
+    }
+
+    const container = this.elementRef.nativeElement.querySelector('.start-menu');
+    if (container && container !== e.target && !container.contains(<any>e.target)) {
+      this.startBarOpened = false;
+    }
+  }
 }
