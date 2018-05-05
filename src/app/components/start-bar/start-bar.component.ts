baa6a2ac1a6d52c076eb7aeaacc6c77bed8dc08c
Jan Kuri <jkuri88@gmail.com>
2018-05-05 22:33:22 +0200

    feat(start): start menu implementation

--- src/app/components/start-bar/start-bar.component.ts
+++ src/app/components/start-bar/start-bar.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start-bar.component.css']
 })
 export class StartBarComponent implements OnInit {
+  startBarOpened: boolean;
 
   constructor() { }
 
